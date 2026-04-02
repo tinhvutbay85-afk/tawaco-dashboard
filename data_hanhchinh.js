5db4da443196deee8958e964e3e49717b22d8649
tinhvutbay85-afk <tinhvutbay85@gmail.com>
2026-04-02 13:36:38 +0700

    Du lieu hanh chinh 02/04/2026 13:36

---
+++ data_hanhchinh.js
@@ -0,0 +1,13 @@
+const TAWACO_HANHCHINH = {
+  "26767": "P.An Phú Đông",
+  "26773": "P.Thới An",
+  "26782": "P.Tân Thới Hiệp",
+  "26785": "P.Trung Mỹ Tây",
+  "26791": "P.Đông Hưng Thuận",
+  "26876": "P.An Nhơn",
+  "26878": "P.An Hội Đông",
+  "26882": "P.An Hội Tây",
+  "26884": "P.Gò Vấp",
+  "26890": "P.Hạnh Thông",
+  "26898": "P.Thông Tây Hội"
+};
