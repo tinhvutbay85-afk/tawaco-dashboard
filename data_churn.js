b2aa46b6e0cb7970baa34d61ebb2fd4b91823fbb
tinhvutbay85-afk <tinhvutbay85@gmail.com>
2026-05-05 15:01:05 +0700

    Cap nhat data Churn KH Lon 05/05/2026 15:00

--- data_churn.js
+++ data_churn.js
@@ -1,9 +1,9 @@
-// TAWACO – Biến động KH Lớn – 13:20:16 5/5/2026
+// TAWACO – Biến động KH Lớn – 14:20:16 5/5/2026
 const TAWACO_CHURN_DATA = {
-  "lastUpdated": "13:20:16 5/5/2026",
+  "lastUpdated": "14:20:16 5/5/2026",
   "kyRange": "K3 -> K4/2026 (Giảm sản lượng KH Lớn)",
-  "tongKH": 3,
-  "tongM3Mat": 653,
+  "tongKH": 4,
+  "tongM3Mat": 734,
   "danhSach": [
     {
       "Danhba": "15112840534",
@@ -31,6 +31,20 @@ const TAWACO_CHURN_DATA = {
       "SL_K3": 638,
       "M3_Giam": 211,
       "TyLe_Giam": 24.85,
+      "Level": "YELLOW"
+    },
+    {
+      "Danhba": "22043304283",
+      "TenKH": "CONG TY CO PHAN XUAT NHAP KHAU CHE BIEN THUC PHAM HOANG THIEN HUONG",
+      "DiaChi": "N/A",
+      "MaDMA": "",
+      "MaPhuong": "26773",
+      "MaGia": "32",
+      "SL_K1": 0,
+      "SL_K2": 502,
+      "SL_K3": 421,
+      "M3_Giam": 81,
+      "TyLe_Giam": 16.14,
       "Level": "YELLOW"
     },
     {
