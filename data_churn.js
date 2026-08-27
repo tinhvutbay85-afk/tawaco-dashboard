e7ef01284aefa29de904d2a74fe5923e42b2eb6f
tinhvutbay85-afk <tinhvutbay85@gmail.com>
2026-08-27 09:01:31 +0700

    Cap nhat data Churn KH Lon 27/08/2026 09:01

--- data_churn.js
+++ data_churn.js
@@ -1,6 +1,6 @@
-// TAWACO – Biến động KH Lớn – 15:20:15 26/8/2026
+// TAWACO – Biến động KH Lớn – 08:53:53 27/8/2026
 const TAWACO_CHURN_DATA = {
-  "lastUpdated": "15:20:15 26/8/2026",
+  "lastUpdated": "08:53:53 27/8/2026",
   "kyRange": "K4 -> K5/2026 (Giảm sản lượng KH Lớn)",
   "tongKH": 43,
   "tongM3Mat": 21671,
